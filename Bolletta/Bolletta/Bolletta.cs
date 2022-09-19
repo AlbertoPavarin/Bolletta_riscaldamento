a24dafff3672809a9ae4cca466265586f65c25b1
AlbertoPavarin <72099114+AlbertoPavarin@users.noreply.github.com>
2022-09-19 18:04:36 +0200

    Classe Bolletta e Riscaldamento

---
+++ Bolletta/Bolletta/Bolletta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolletta
+{
+    class Bolletta
+    {
+        protected double spesaMateria;
+        protected int spesaTrasGestCont;
+        protected int spesaOneri;
+        protected int qvd;
+        protected double totale;
+
+        public Bolletta()
+        {
+            this.spesaMateria = 0;
+            this.spesaTrasGestCont = 96;
+            this.spesaOneri = 47;
+            this.qvd = 70;
+        }
+
+        public void SetSpesaMateria(double spesa)
+        {
+            this.spesaMateria = spesa;
+        }
+
+        public string GetSpesaMateria()
+        {
+            return $"Spesa materia {this.spesaMateria}";
+        }
+
+        public override string ToString()
+        {
+            return $"Bolletta: {this.spesaMateria + this.spesaOneri + this.spesaTrasGestCont + this.qvd}€";
+        }
+
+        public void CalcolaBolletta()
+        {
+            this.totale = this.spesaMateria + this.spesaOneri + this.spesaTrasGestCont + this.qvd;
+        }
+
+        public double GetTotale()
+        {
+            return this.totale;
+        }
+    }
+}
---
+++ Bolletta/Bolletta/Riscaldamento.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolletta
+{
+    class Riscaldamento
+    {
+        protected double rendimento;
+        protected string tipoConsumo;
+        protected double costoAnnuale;
+        protected double consumi;
+        protected double totale;
+
+        public Riscaldamento()
+        {
+            
+        }
+
+        public void SetRendimento(double rendimento)
+        {
+            this.rendimento = rendimento;
+        }
+
+        public void setTipoConsumo(string tipoCons)
+        {
+            this.tipoConsumo = tipoCons;
+        }
+
+        public void setCostoAnnuale(double costoAnn)
+        {
+            this.costoAnnuale = costoAnn;
+        }
+
+        public void SetConsumi(double cons)
+        {
+            this.consumi = cons;
+        }
+
+        public double GetRendimento()
+        {
+            return this.rendimento;
+        }
+
+        public string GetTipoConsumo()
+        {
+            return $"{this.tipoConsumo}";
+        }
+
+        public double GetCostoAttuale()
+        {
+            return this.costoAnnuale;
+        }
+
+        public double GetConsumi()
+        {
+            return this.consumi;
+        }
+
+        public override string ToString()
+        {
+            return $"Il rendimento del modello è: {this.rendimento}, il tipo di consumo è: {this.tipoConsumo}, il costo annuale è: {this.costoAnnuale} e i consumi sono: {this.consumi}";
+        }
+
+        public void CalcolaCostoTotale() 
+        {
+            this.totale = this.costoAnnuale * this.consumi;
+        }
+
+        public double GetTotale()
+        {
+            return this.totale;
+        }
+    }
+}
